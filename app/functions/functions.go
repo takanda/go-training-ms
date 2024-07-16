13bd1b213ee6d634f8914d87666c97afa00181cc
takanda <takanda170@gmail.com>
2024-07-16 14:14:54 +0900

    Add functions

--- app/functions/functions.go
+++ app/functions/functions.go
@@ -1,13 +1,30 @@
 package functions
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
+import "fmt"
 
 func ShowFunctions() {
-	number1, _ := strconv.Atoi(os.Args[1])
-	number2, _ := strconv.Atoi(os.Args[2])
-	fmt.Println("Sum:", number1+number2)
+	// sum, mul := calc(os.Args[1], os.Args[2])
+	// fmt.Println("Sum:", sum)
+	// fmt.Println("Mul:", mul)
+	firstName := "John"
+	fmt.Println(firstName)
+	updateName(&firstName)
+	fmt.Println(firstName)
+}
+
+// func calc(number1 string, number2 string) (sum int, mul int) {
+// 	int1, _ := strconv.Atoi(number1)
+// 	int2, _ := strconv.Atoi(number2)
+// 	sum = int1 + int2
+// 	mul = int1 * int2
+// 	return 
+// }
+
+func updateName(name *string) {
+	*name = "David"
 }
