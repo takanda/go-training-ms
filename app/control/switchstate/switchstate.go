2c416fa5fdf0d05f36068d12abe4b2bfac60d623
takanda <takanda170@gmail.com>
2024-07-18 22:09:17 +0900

    Add switch

--- app/control/switchstate/switchstate.go
+++ app/control/switchstate/switchstate.go
@@ -1,23 +1,29 @@
 package switchstate
 
 import (
-	"math/rand"
 	"time"
 )
 
 func Switchstate() string {
-	sec := time.Now().Unix()
-	rand.Seed(sec)
-	i := rand.Int31n(10)
+	// sec := time.Now().Unix()
+	// rand.Seed(sec)
+	// i := rand.Int31n(10)
 
-	switch i {
-	case 0:
-		return "zero"
-	case 1:
-		return "one"
-	case 2:
-		return "two"
-	default:
-		return "no match"
+	// switch i {
+	// case 0:
+	// 	return "zero"
+	// case 1:
+	// 	return "one"
+	// case 2:
+	// 	return "two"
+	// default:
+	// 	return "no match"
+	// }
+
+	switch time.Now().Weekday().String() {
+		case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+			return "Weekday!"
+		default:
+			return "Weekend!"
 	}
 }
