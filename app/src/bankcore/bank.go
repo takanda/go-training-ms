df9db14e348606a26dfc383c09886f1e7847bff6
takanda <takanda170@gmail.com>
2024-07-31 21:05:57 +0900

    Add bankcore func deposit and withdraw

--- app/src/bankcore/bank.go
+++ app/src/bankcore/bank.go
@@ -1,5 +1,7 @@
 package bank
 
+import "errors"
+
 type Customer struct {
 	Name    string
 	Address string
@@ -10,4 +12,25 @@ type Account struct {
 	Customer
 	Number  int32
 	Balance float64
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to deposit should be greater than zero")
+	}
+	a.Balance += amount
+	return nil
+}
+
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to withdraw should be greater than zero")
+	}
+
+	if a.Balance < amount {
+        return errors.New("the amount to withdraw should be greater than the account's balance")
+    }
+	
+	a.Balance -= amount
+    return nil
 }
